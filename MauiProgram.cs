8b47064df4cc11222be1b70ace798e4b46b74c0a
Mungert69 <contact@mahadeva.co.uk>
2025-10-24 20:56:34 +0000

    added debug

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -54,7 +54,7 @@ namespace NetworkMonitorAgent
                 builder.Services.AddLogging(loggingBuilder =>
                 {
                     loggingBuilder.ClearProviders(); // Optional: Clears default providers if necessary
-                    loggingBuilder.SetMinimumLevel(LogLevel.Information); // Set the minimum log level
+                    loggingBuilder.SetMinimumLevel(LogLevel.Debug); // Set the minimum log level
                     loggingBuilder.AddConsole(); // Console logger (useful for debugging)
                     loggingBuilder.AddDebug();   // Debug output window (useful in Visual Studio)
 
